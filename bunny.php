ad8bb438df6925f8ab162acd0e6fe79e91c37e4b
pardhu avula <avulapardhu07@gmail.com>
2025-01-28 15:37:20 +0530

    first commit

---
+++ bunny.php
@@ -0,0 +1,12 @@
+<?php
+$score = 60;
+if ($score >= 90) {
+  echo "Grade: A";
+} elseif ($score >= 80) {
+  echo "Grade: B";
+} elseif ($score >= 70) {
+  echo "Grade: C";
+} else {
+  echo "Grade: F";
+}
+?>  v v v v v v v v v v v v v v v v v v v v v v 
